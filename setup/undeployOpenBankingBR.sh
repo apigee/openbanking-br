4eeb7af5924858e301f5edaa78c732ac640d89b1
Debora Elkin <deboraelkin@google.com>
2022-08-29 15:50:54 +1000

    Added removal of products, kvms and caches

--- setup/undeployOpenBankingBR.sh
+++ setup/undeployOpenBankingBR.sh
@@ -26,8 +26,8 @@ function set_cred(){
     if [[ "$1" == *"delete"* ]]; then
     env=""
     fi
-    local apigee_user_cred="-u $APIGEE_USER -p $APIGEE_PASSWORD -o $APIGEE_ORG"
-    local apigee_token_cred="-t $APIGEE_TOKEN -o $APIGEE_ORG"
+    local apigee_user_cred="-u $APIGEE_USER -p $APIGEE_PASSWORD -o $APIGEE_ORG -e $APIGEE_ENV"
+    local apigee_token_cred="-t $APIGEE_TOKEN -o $APIGEE_ORG -e $APIGEE_ENV" 
     if [[ -z "${APIGEE_TOKEN}" ]]; then
         to_eval="${to_eval} ${apigee_user_cred}"
     else
@@ -36,25 +36,65 @@ function set_cred(){
     to_eval="${to_eval}${env} ${suffix}"
     eval "$to_eval"
 }
+# Credentials settings helper function for org level config
+function set_cred_org(){
+    to_eval=$1
+    local suffix=$2
+    local apigee_user_cred="-u $APIGEE_USER -p $APIGEE_PASSWORD -o $APIGEE_ORG"
+    local apigee_token_cred="-t $APIGEE_TOKEN -o $APIGEE_ORG"
+    if [[ -z "${APIGEE_TOKEN}" ]]; then
+        to_eval="${to_eval} ${apigee_user_cred}"
+    else
+        to_eval="${to_eval} ${apigee_token_cred}"
+    fi
+    to_eval="${to_eval} ${suffix}"
+    eval "$to_eval"
+}
+
+
+
+# Remove OBBR Test Client app
+# If no developer name has been set, use a default
+if [ -z "$OBBR_TEST_DEVELOPER_EMAIL" ]; then  OBBR_TEST_DEVELOPER_EMAIL=OBBR-Test-Developer@somefictitioustestcompany.com; fi;
+echo "--->"  Removing Test App: OOBRTestApp...
+set_cred_org "apigeetool deleteApp" " --email $OBBR_TEST_DEVELOPER_EMAIL --name OBBRTestApp" 
+
+# Remove OBBR Test Developer
+echo "--->"  Removing Test Developer: $OBBR_TEST_DEVELOPER_EMAIL
+set_cred_org "apigeetool deleteDeveloper" "--email $OBBR_TEST_DEVELOPER_EMAIL"
+
+
+# Remove Products
+echo "--> Removing Products"
+echo "--->"  Removing API Product: "Accounts"
+set_cred_org "apigeetool deleteProduct" "--productName \"OBBRAccounts\"  "
+echo "--->"  Removing API Product: "OIDC"
+set_cred_org "apigeetool deleteProduct" "--productName \"OBBROIDC\"  "
+echo "--->"  Removing API Product: "Consents"
+set_cred_org "apigeetool deleteProduct" "--productName \"OBBRConsents\"  "
+echo "--->"  Removing API Product: "Resources"
+set_cred_org "apigeetool deleteProduct" "--productName \"OBBRResources\"  "
+echo "--->"  Removing API Product: "Admin"
+set_cred_org "apigeetool deleteProduct" "--productName \"OBBRAdmin\"  "
 
 # Deploy authnz related Proxies
-cd ../src/apiproxies/authnz
+cd src/apiproxies/authnz
 for ap in $(ls .) 
 do 
     echo "--->" Undeploying $ap Apiproxy
     cd $ap
     set_cred "apigeetool undeploy" "-n $ap"
     echo "--->" Deleting $ap Apiproxy
-    set_cred "apigeetool delete" "-n $ap"
+    set_cred_org "apigeetool delete" "-n $ap"
     cd ..
  done
 
-# Uneploy utils Proxies
+# Undeploy utils Proxies
 cd ../utils/mock-obbr-client
 echo "--->"  Undeploying mock-obbr-client Apiproxy
 set_cred "apigeetool undeploy" "-n mock-obbr-client"
 echo "--->" Deleting mock-obbr-client Apiproxy
-set_cred "apigeetool delete" "-n mock-obbr-client"
+set_cred_org "apigeetool delete" "-n mock-obbr-client"
 cd ..
 
 # Undeploy OBBR-Admin proxy
@@ -62,7 +102,8 @@ cd ../admin/OBBR-Admin
 echo "--->" Undeploying OBBR-Admin Apiproxy
 set_cred "apigeetool undeploy" "-n OBBR-Admin"
 echo "--->" Deleting OBBR-Admin Apiproxy
-set_cred "apigeetool delete" "-n OBBR-Admin"
+set_cred_org "apigeetool delete" "-n OBBR-Admin"
+cd ..
 
  # Undeploy common apiproxies
 cd ../common
@@ -72,7 +113,7 @@ do
     cd $ap
     set_cred "apigeetool undeploy" "-n $ap"
     echo "--->" Deleting $ap Apiproxy
-    set_cred "apigeetool delete" "-n $ap"
+    set_cred_org "apigeetool delete" "-n $ap"
     cd ..
  done
 
@@ -84,21 +125,41 @@ do
     cd $ap
     set_cred "apigeetool undeploy" "-n $ap"
     echo "--->" Deleting $ap Apiproxy
-    set_cred "apigeetool delete" "-n $ap"
+    set_cred_org "apigeetool delete" "-n $ap"
     cd ..
  done
 
 # Undeploy Shared flows
-cd ../../../shared-flows
+cd ../../shared-flows
 for sf in $(ls .) 
 do 
     echo "--->" Undeploying $sf Shared Flow 
     cd $sf
     set_cred "apigeetool undeploySharedflow" "-n $sf"
     echo "--->" Deleting $sf Shared Flow 
-    set_cred "apigeetool deleteSharedflow" "-n $sf"
+    set_cred_org "apigeetool deleteSharedflow" "-n $sf"
     cd ..
 done
+
+# Remove KVM Maps
+echo "--> Removing Key Value Maps"
+echo "--->"  Removing KVM mockOBBRClient...
+set_cred "apigeetool deletekvmmap" "--mapName mockOBBRClient"
+echo "--->"  Removing KVM Consents...
+set_cred "apigeetool deletekvmmap" "--mapName Consents"
+echo "--->"  Removing KVM TokensIssuedForConsent...
+set_cred "apigeetool deletekvmmap" "--mapName TokensIssuedForConsent"
+echo "--->"  Removing KVM OBBRConfig...
+set_cred "apigeetool deletekvmmap" "--mapName OBBRConfig"
+
+# Remove caches
+echo "--> Removing Caches"
+echo "--->"  Removing cache OIDCState...
+set_cred "apigeetool deletecache" "-z OIDCState"
+echo "--->"  Removing cache PushedAuthReqs...
+set_cred "apigeetool deletecache" "-z PushedAuthReqs"
+echo "--->"  Removing cache ConsentState...
+set_cred "apigeetool deletecache" "-z ConsentState"
 
 # Revert to original directory
 cd ../..
